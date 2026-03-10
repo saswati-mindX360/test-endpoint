d504f9219073cc913df736e3438a433c7d77c60a
saswati-mindX360 <saswati.choudhury@mindx360.com>
2026-03-10 17:25:29 +0530

    first commit

---
+++ backend/src/main/java/com/app/endpointcopy/EndpointCopyApplication.java
@@ -0,0 +1,14 @@
+package com.app.endpointcopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointCopyApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Working fine...");
+        SpringApplication.run(EndpointCopyApplication.class, args);
+    }
+
+}
